cc36f507182345df2acd5762ad9352910af0e481
nalin1997 <nalin.luthra@gmail.com>
2019-03-24 03:02:08 +0530

    Still trying

--- final_firebase_data.py
+++ final_firebase_data.py
@@ -21,21 +21,35 @@ ref.set({
                     'color': 'red',
                     'Status': 'OFF',
                     'power': 40,
-                    'length': 2
+                    #'length': 2
                 },
                 'Bulb': {
                     'color': 'green',
                     'Status': 'OFF',
                     'power': 100,
-                    'length': 3
+                    #'length': 3
                 },
                 'Hair Dryer': {
                     'color': 'yellow',
                     'Status': 'OFF',
-                    'height': 1000,
-                    'length': 1
+                    'power': 1000,
+                    #'length': 1
                 }
             }
         })
 
 ser = serial.Serial('/dev/ttyACM1', 9600, timeout=0)
+
+cur = 0
+
+while 1:
+	try:
+		current = ser.readline()
+    	difference = current - cur
+    	if(difference>30 && difference<50):
+      		#Update Data Values
+      		ref = db.reference('boxes')
+			box_ref = ref.child('box001')
+			box_ref.update({
+			    'color': 'blue'
+			})
